c07a5a77d58f63a22a889c2ea947510fa52c10b6
svenkatesh009 <117891450+svenkatesh009@users.noreply.github.com>
2023-05-11 09:22:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args){
+        RWOnly ob=new RWOnly();
+        ob.setting("Ram");
+        System.out.println(ob.getting());
+    }
 }
