a7a45e1e43b25df87789d0c70e5596e74f206677
svenkatesh009 <117891450+svenkatesh009@users.noreply.github.com>
2023-05-11 09:24:01 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setting(String name){
+        this.name=name;
+    }
+    public String getting(){
+        return this.name;
+    }
 }
